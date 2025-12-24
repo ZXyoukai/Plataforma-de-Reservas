b986fadf77c3e0dcbc5a6449f1faee7344723f52
ZXyoukai <martinhocarneiros@gmail.com>
2025-12-25 00:31:37 +0100

    refactor: reestruturar os módulos e serviços da aplicação num formato modular

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../controllers/app.controller';
+import { AppService } from '../services/app.service';
 import { AuthModule } from './auth/auth.module';
 import { ManageModule } from './manage/manage.module';
 import { ReservationModule } from './reservation/reservation.module';
+
 
 @Module({
   imports: [AuthModule, ManageModule, ReservationModule],
