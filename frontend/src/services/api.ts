2c8468d6320ac283703eb0d6e73bae81691df9d9
Domingos Germano Franco <dgermano@student.42luanda.com>
2025-12-30 12:28:43 +0100

    Update API base URL for service

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://plataforma-de-reservas-zz97.onrender.com';
+const API_BASE_URL = 'https://plataforma-de-reservas-7n8l.vercel.app';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
